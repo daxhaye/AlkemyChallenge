a17dbe5d4a052026839c11e21276bc7356629b52
Jere Pfluger <jerepfluger@gmail.com>
2021-06-05 00:10:28 +0200

    Update StudentController.java

--- src/main/java/com/springboot/universidad/app/controllers/StudentController.java
+++ src/main/java/com/springboot/universidad/app/controllers/StudentController.java
@@ -15,43 +15,63 @@ import com.springboot.universidad.app.models.entity.Subjects;
 
 @Controller
 public class StudentController {
-	
-	@Autowired
-	private ISubjectsDao subjectDao;
-	
-	@Autowired
-	private IStudentDao studentDao;
-	
-	@GetMapping("/inscribirse/{id}")
-	public String inscribirse(@PathVariable(value = "id") int id, RedirectAttributes flash, Principal principal) {
-		Student alumno = null;
-		
-		if(principal != null) {
-			alumno = studentDao.findByUsername(principal.getName());
-			
-			Subjects materia = null;
-			if (id>0) {
-				materia = subjectDao.findOne(id);
-				if(!alumno.getMaterias().contains(materia) && materia.getCupAlumn() > 0) {
-					materia.decCupAlumn();
-					alumno.addMaterias(materia);
-					studentDao.save(alumno);
-					subjectDao.save(materia);
-					flash.addFlashAttribute("success", "Te inscribiste a " + materia.getName() + " correctamente!");
-				} else {
-					String mensajeFlash = (alumno.getMaterias().contains(materia)) ? "Ya estas inscripto en la materia" : "Cupo lleno";
-					flash.addFlashAttribute("error", mensajeFlash);
-				}
-			} else {
-				flash.addFlashAttribute("Error", "El Id no puede ser 0");
-			}
-					
-		} else {
-			flash.addFlashAttribute("error", "No ha iniciado Sesión");
-		}
-		
-		
-		return "redirect:/ver/" + id;
-	}
+    // Clave el uso de constantes
+    private final static String ERROR = "Error";
 
+    @Autowired
+    private ISubjectsDao subjectDao;
+
+    @Autowired
+    private IStudentDao studentDao;
+
+    @GetMapping("/inscribirse/{id}")
+    public String inscribirse(@PathVariable(value = "id") int id, RedirectAttributes flash, Principal principal) {
+        String result = "redirect:/ver/" + id;
+        // La iniciación de variables en null olvidate. Objeto loquesea = null, solo escribi Objeto loquesea
+        Student alumno;
+
+        // Esto se llama fail fast.. lo primero que haces es devolver el caso de error
+        // Entonces despues el caso principal es mas facil de leer ;)
+        if (principal == null) {
+            flash.addFlashAttribute(ERROR, "No ha iniciado Sesión");
+
+            return result;
+        }
+
+        alumno = studentDao.findByUsername(principal.getName());
+
+        Subjects materia;
+        // De nuevo, fail fast. Primero el caso de error
+        if (id <= 0) {
+            flash.addFlashAttribute(ERROR, "El Id no puede ser 0");
+
+            return result;
+        }
+
+        materia = subjectDao.findOne(id);
+        // Evita SIEMPRE las logicas negadas. Es muy propenso a pegarte un tiro en las pelotas.
+        // Ahora porque tenes el codigo muy fresquito. Despues cuando lo leas en 2 meses no vas a entender una chota
+        // Esto se asemeja un poco al tema de "fail fast"
+        if (alumno.getMaterias().contains(materia) && materia.getCupAlumn() > 0) {
+            String mensajeFlash = (alumno.getMaterias().contains(materia)) ? "Ya estas inscripto en la materia" : "Cupo lleno";
+            flash.addFlashAttribute("error", mensajeFlash);
+        } else {
+            materia.decCupAlumn();
+            alumno.addMaterias(materia);
+            studentDao.save(alumno);
+            subjectDao.save(materia);
+            // La concatenacion de strings no se hace NUNCA con el + (en ningun lenguaje)
+            flash.addFlashAttribute("success", String.format("Te inscribiste a %s correctamente!", materia.getName()));
+        }
+
+        return result;
+        // No me gusta como quedo esto de "return result" por todos lados, pero era para mostrarte un poco como es la bocha
+    }
 }
+
+// Por favor te lo pido por lo que mas quieras en el mundo.
+// NUNCA uses tabs, siempre espacios ;) y siempre trata de formatear bien los archivos
+// existe algo que se llama "linter" que te autoformatea los archivos en casi todos los editores de texto
+// por ejemplo en intellij se usa "ctrl + shift + l" y lo corre solito nomas
+// basicamente lo que hace es pasar de "if(davidEsGay.equals("noSabeUsarLinter)){
+// a algo como "if (davidEsGay.equals("noSabeUsarLinter")) { ...... los espacios son claves ;)
